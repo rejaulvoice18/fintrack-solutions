7cced89e75bf9b89f9de2e58feba343f6436382b
rejaul <rejaulvoice@gmail.com>
2025-01-17 17:53:45 +0000

    create client site for the website of fintrack-solutions and as well create several pages

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'JetBrains Mono, system-ui, sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
