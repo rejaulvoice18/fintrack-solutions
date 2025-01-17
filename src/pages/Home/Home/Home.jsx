3a1f4b9b0aeab195bf58fe81e7e35c46acb5f485
rejaul <rejaulvoice@gmail.com>
2025-01-17 18:35:27 +0000

    added authProvider functionality

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+
 
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">Home</h2>
+            <h2>HOme</h2>
         </div>
     );
 };
