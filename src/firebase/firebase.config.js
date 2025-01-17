710a7952e33c18d5a77686a22a830993c8dec34f
rejaul <rejaulvoice@gmail.com>
2025-01-17 18:04:16 +0000

    added firebase credentials

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOBiVGMDDq-MXE-72hrPNbygbptXS2dbw",
+  authDomain: "fintrack-solutions.firebaseapp.com",
+  projectId: "fintrack-solutions",
+  storageBucket: "fintrack-solutions.firebasestorage.app",
+  messagingSenderId: "280073492354",
+  appId: "1:280073492354:web:43b0fd24efdd14e554619b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
