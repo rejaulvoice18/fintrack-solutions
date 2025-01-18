775fb3eda8bd3bb4390aa741fdf8fa9cac5f3610
rejaul <rejaulvoice@gmail.com>
2025-01-18 20:03:42 +0000

    Added functionality to store the user data to the database

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,13 +2,73 @@ import Lottie from 'lottie-react';
 import registerLottieData from '../../assets/lottie/register.json'
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
+import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
+import { useState } from 'react';
+
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY
+// image hosting url making
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const Register = () => {
-    const { register, handleSubmit, reset, watch, formState: { errors } } = useForm();
+    const axiosPublic = useAxiosPublic()
+    const [error, setError] = useState('')
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { createUser, updateUserProfile } = useAuth();
+    const navigate = useNavigate()
 
-    const onSubmit = data => {
+    const onSubmit = async (data) => {
+        console.log(data)
+       // image upload to imgbb and then get an url
+       const imageFile = { image: data.image[0] }
+       const res = await axiosPublic.post(image_hosting_api, imageFile, {
+           headers: {
+               'content-type': 'multipart/form-data'
+           }
+       })
+        // console.log(res.data.data.display_url)
 
+        createUser(data.email, data.password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser)
+
+                // updating user profile
+                updateUserProfile(data.name, res.data.data.display_url)
+                    .then(async () => {
+                        if (res.data.success) {
+                            // create user info in the database
+                            const userInfo = {
+                                name: data.name,
+                                email: data.email,
+                                bank_account_no: data.bank_account_no,
+                                salary: data.salary,
+                                role: data.user_role,
+                                designation: data.designation,
+                                image: res.data.data.display_url
+
+                            }
+                            // saving data to the server
+                            const userData = await axiosPublic.post('/users', userInfo);
+                            console.log('with image url', userData.data)
+                            if (userData.data.insertedId) {
+                                Swal.fire({
+                                    position: "top-end",
+                                    icon: "success",
+                                    title: 'Account create successfully',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                });
+                                navigate('/')
+                            }
+                        }
+                    })
+
+            })
+            .then(err => console.log(err))
     }
 
     return (
@@ -40,8 +100,9 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Bank Account Number</span>
                             </label>
-                            <input type="number" {...register("bank_account_no", { required: true })} name="bank_account_no" placeholder="Bank Account Number" className="input input-bordered" />
+                            <input type="number" {...register("bank_account_no", { required: true, minLength: 10 })} name="bank_account_no" placeholder="Bank Account Number" className="input input-bordered" />
                             {errors.bank_account_no && <p className='text-red-600'>Bank Account is required</p>}
+                            {errors.bank_account_no?.type === 'minLength' && <p className='text-red-600'>Account must be more than 10 digits</p>}
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -71,12 +132,9 @@ const Register = () => {
                                 <option value="hr">HR</option>
                             </select>
                         </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Photo URL</span>
-                            </label>
-                            <input type="text" {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
-                            {errors.photoURL && <p className='text-red-600'>Photo URL is required</p>}
+                        <div className='form-control w-full'>
+                            <input {...register('image', { required: true })} type="file"
+                                className="file-input w-full max-w-xs " />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -95,6 +153,7 @@ const Register = () => {
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
+                            <p className='text-red-500'>{error}</p>
                         </div>
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">SignUp</button>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,14 +1,32 @@
 import { createContext, useState } from "react";
-
+import { createUserWithEmailAndPassword, updateProfile,  } from 'firebase/auth';
+import auth from "../firebase/firebase.config";
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
+   
+
+    // creating user
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+
+    // update user info
+    const updateUserProfile = (name, photo) =>{
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo 
+        });
+    }
 
     const authInfo = {
         user,
         loading,
+        createUser,
+        updateUserProfile
     }
 
     return (
